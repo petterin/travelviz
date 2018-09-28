1bbb84db0f9bdba0187828b625ea99458b774f78
Petteri Noponen <petteri.noponen@gmail.com>
2018-09-28 17:08:26 +0300

    Add Firebase SDK and initial locations points to map

--- src/Map/JourneyMap.js
+++ src/Map/JourneyMap.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import Leaflet from "leaflet";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  CircleMarker,
+  Map as LeafletMap,
+  Marker,
+  Popup,
+  TileLayer,
+  Tooltip
+} from "react-leaflet";
+
+import { initFirebase, initFirestore } from "../common/firebaseHelpers";
 
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import mockLocations from "./mock-locations.json";
 
-import marker from 'leaflet/dist/images/marker-icon.png';
-import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import marker from "leaflet/dist/images/marker-icon.png";
+import marker2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 // This workaround allows us to use Leaflet icons from npm with Webpack
 // (https://github.com/PaulLeCam/react-leaflet/issues/255)
@@ -22,23 +32,60 @@ class JourneyMap extends Component {
   state = {
     lat: 60.1869,
     lng: 24.8215,
-    zoom: 14
+    zoom: 3,
+    locations: [],
+    locationsLoading: false
   };
 
+  componentDidMount() {
+    // Set location data from temporary source:
+    this.setState({ locations: mockLocations.locations });
+
+    // (Following is commented out because this version would
+    // consume Firebase's daily free quota very fast!)
+    /*
+    const firebase = initFirebase();
+    const db = initFirestore(firebase);
+
+    this.setState({ locationsLoading: true });
+    db.collection("locations")
+      .get()
+      .then(snapshot => {
+        const locations = [];
+        snapshot.forEach(doc => locations.push(doc.data()));
+        this.setState({ locations: locations, locationsLoading: false });
+      })
+      .catch(err => {
+        console.log("Error getting documents:", err);
+        this.setState({ locationsLoading: false });
+      });
+    */
+  }
+
   render() {
+    const { locations, locationsLoading, zoom } = this.state;
     const position = [this.state.lat, this.state.lng];
+    const locationMarkers = locations.map(loc => (
+      <CircleMarker
+        center={[loc.coords.latitude, loc.coords.longitude]}
+        key={loc.timestamp}
+      >
+        <Tooltip>{new Date(loc.timestamp).toString()}</Tooltip>
+      </CircleMarker>
+    ));
     return (
-      <Map className="JourneyMap" center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
+      <div className="JourneyMap-container">
+        <LeafletMap className="JourneyMap" center={position} zoom={zoom}>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>This is an example popup.</Popup>
+          </Marker>
+          <React.Fragment>{locationMarkers}</React.Fragment>
+        </LeafletMap>
+      </div>
     );
   }
 }
