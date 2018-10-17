bc06dc55055cf99e873317938566f6b88ef727ce
Petteri Noponen <petteri.noponen@gmail.com>
2018-10-17 03:57:19 +0300

    Add locally stored versions of GPS data points

--- scripts/convertGarminData.js
+++ scripts/convertGarminData.js
@@ -7,52 +7,96 @@
 const fs = require("fs");
 
 // TODO: Change these:
-const inputPath = "garmin_data1.json";
-const outputPath = "garmin_data1_converted.json";
+const inputPaths = [
+  "garmin_3069518268.json",
+  "garmin_3069512763.json",
+  "garmin_3066921966.json",
+  "garmin_3066921799.json",
+  "garmin_3064392533.json",
+  "garmin_3061476113.json",
+  "garmin_3059461791.json",
+  "garmin_3056121999.json",
+  "garmin_3056121805.json",
+  "garmin_3037698901.json",
+  "garmin_3032006437.json",
+  "garmin_3028657078.json",
+  "garmin_3024610852.json",
+  "garmin_3024610723.json",
+  "garmin_3024610671.json",
+  "garmin_3024610488.json",
+  "garmin_3024610111.json",
+  "garmin_3024610027.json",
+  "garmin_3008099722.json",
+  "garmin_3008099695.json",
+  "garmin_3008099669.json",
+  "garmin_3008099631.json",
+  "garmin_3008099608.json",
+  "garmin_3008099590.json",
+  "garmin_2994808167.json",
+  "garmin_2987001621.json",
+  "garmin_2984978172.json",
+  "garmin_2984583396.json",
+  "garmin_2982840709.json",
+  "garmin_2982250804.json",
+  "garmin_2981964669.json",
+  "garmin_2974854277.json",
+  "garmin_2974433520.json",
+  "garmin_2972158314.json",
+  "garmin_2972158235.json",
+  "garmin_2969499683.json",
+  "garmin_2969310247.json"
+];
+const outputDir = "../src/data/";
 
-const file = fs.readFileSync(inputPath, "utf8");
-const json = JSON.parse(file);
-
-// input field name -> output field name. (Commented out / undefined = Not included in output)
-const outputMetricKeys = {
-  // sumElapsedDuration: "elapsedDuration",
-  // sumMovingDuration: "movingDuration",
-  directLongitude: "longitude",
-  // sumDistance: "sumDistance",
-  directAirTemperature: "airTemperature",
-  directTimestamp: "timestamp",
-  // sumDuration: "duration",
-  directCorrectedElevation: "correctedElevation",
-  directLatitude: "latitude",
-  // directUncorrectedElevation: "elevation",
-  directSpeed: "speed",
-  directElevation: "elevation",
-  directVerticalSpeed: "verticalSpeed"
-};
-
-const metrics = [];
-json.metricDescriptors.forEach(m => {
-  const outputKey = outputMetricKeys[m.key];
-  if (outputKey !== undefined) {
-    metrics[m.metricsIndex] = outputKey || m.key;
-  }
+inputPaths.forEach(inputPath => {
+  convert(inputPath, `${outputDir}${inputPath}`);
 });
 
-const convertMetricsArray = m => {
-  const metricsObj = {};
-  m.metrics.forEach((value, i) => {
-    const key = metrics[i];
-    if (key === "timestamp") {
-      metricsObj[key] = new Date(value).toISOString();
-    } else if (key) {
-      metricsObj[key] = value;
+function convert(inputPath, outputPath) {
+  const file = fs.readFileSync(inputPath, "utf8");
+  const json = JSON.parse(file);
+
+  // input field name -> output field name. (Commented out / undefined = Not included in output)
+  const outputMetricKeys = {
+    // sumElapsedDuration: "elapsedDuration",
+    // sumMovingDuration: "movingDuration",
+    directLongitude: "longitude",
+    sumDistance: "sumDistance",
+    directAirTemperature: "airTemperature",
+    directTimestamp: "timestamp",
+    // sumDuration: "duration",
+    directCorrectedElevation: "correctedElevation",
+    directLatitude: "latitude",
+    // directUncorrectedElevation: "elevation",
+    directSpeed: "speed",
+    directElevation: "elevation",
+    directVerticalSpeed: "verticalSpeed"
+  };
+
+  const metrics = [];
+  json.metricDescriptors.forEach(m => {
+    const outputKey = outputMetricKeys[m.key];
+    if (outputKey !== undefined) {
+      metrics[m.metricsIndex] = outputKey || m.key;
     }
   });
-  return metricsObj;
-};
-const data = json.activityDetailMetrics.map(convertMetricsArray);
-const outputJson = { garminActivityId: json.activityId, locations: data };
 
-const output = JSON.stringify(outputJson);  
-fs.writeFileSync(outputPath, output);  
-console.log(`Wrote ${data.length} datapoints to file ${outputPath}.`);
+  const convertMetricsArray = m => {
+    const metricsObj = {};
+    m.metrics.forEach((value, i) => {
+      const key = metrics[i];
+      if (key === "timestamp") {
+        metricsObj[key] = new Date(value).toISOString();
+      } else if (key) {
+        metricsObj[key] = value;
+      }
+    });
+    return metricsObj;
+  };
+  const data = json.activityDetailMetrics.map(convertMetricsArray);
+  const outputJson = { garminActivityId: json.activityId, locations: data };
+
+  const output = JSON.stringify(outputJson);
+  fs.writeFileSync(outputPath, output);
+  console.log(`Wrote ${data.length} datapoints to file ${outputPath}.`);
+}
---
+++ src/data/garmin_locations.js
@@ -0,0 +1,81 @@
+import garmin_3069518268 from "./garmin_3069518268.json";
+import garmin_3069512763 from "./garmin_3069512763.json";
+import garmin_3066921966 from "./garmin_3066921966.json";
+import garmin_3066921799 from "./garmin_3066921799.json";
+import garmin_3064392533 from "./garmin_3064392533.json";
+import garmin_3061476113 from "./garmin_3061476113.json";
+import garmin_3059461791 from "./garmin_3059461791.json";
+import garmin_3056121999 from "./garmin_3056121999.json";
+import garmin_3056121805 from "./garmin_3056121805.json";
+import garmin_3037698901 from "./garmin_3037698901.json";
+import garmin_3032006437 from "./garmin_3032006437.json";
+import garmin_3028657078 from "./garmin_3028657078.json";
+import garmin_3024610852 from "./garmin_3024610852.json";
+import garmin_3024610723 from "./garmin_3024610723.json";
+import garmin_3024610671 from "./garmin_3024610671.json";
+import garmin_3024610488 from "./garmin_3024610488.json";
+import garmin_3024610111 from "./garmin_3024610111.json";
+import garmin_3024610027 from "./garmin_3024610027.json";
+import garmin_3008099722 from "./garmin_3008099722.json";
+import garmin_3008099695 from "./garmin_3008099695.json";
+import garmin_3008099669 from "./garmin_3008099669.json";
+import garmin_3008099631 from "./garmin_3008099631.json";
+import garmin_3008099608 from "./garmin_3008099608.json";
+import garmin_3008099590 from "./garmin_3008099590.json";
+import garmin_2994808167 from "./garmin_2994808167.json";
+import garmin_2987001621 from "./garmin_2987001621.json";
+import garmin_2984978172 from "./garmin_2984978172.json";
+import garmin_2984583396 from "./garmin_2984583396.json";
+import garmin_2982840709 from "./garmin_2982840709.json";
+import garmin_2982250804 from "./garmin_2982250804.json";
+import garmin_2981964669 from "./garmin_2981964669.json";
+import garmin_2974854277 from "./garmin_2974854277.json";
+import garmin_2974433520 from "./garmin_2974433520.json";
+import garmin_2972158314 from "./garmin_2972158314.json";
+import garmin_2972158235 from "./garmin_2972158235.json";
+import garmin_2969499683 from "./garmin_2969499683.json";
+import garmin_2969310247 from "./garmin_2969310247.json";
+
+const garminActivities = [
+  garmin_3069518268,
+  garmin_3069512763,
+  garmin_3066921966,
+  garmin_3066921799,
+  garmin_3064392533,
+  garmin_3061476113,
+  garmin_3059461791,
+  garmin_3056121999,
+  garmin_3056121805,
+  garmin_3037698901,
+  garmin_3032006437,
+  garmin_3028657078,
+  garmin_3024610852,
+  garmin_3024610723,
+  garmin_3024610671,
+  garmin_3024610488,
+  garmin_3024610111,
+  garmin_3024610027,
+  garmin_3008099722,
+  garmin_3008099695,
+  garmin_3008099669,
+  garmin_3008099631,
+  garmin_3008099608,
+  garmin_3008099590,
+  garmin_2994808167,
+  garmin_2987001621,
+  garmin_2984978172,
+  garmin_2984583396,
+  garmin_2982840709,
+  garmin_2982250804,
+  garmin_2981964669,
+  garmin_2974854277,
+  garmin_2974433520,
+  garmin_2972158314,
+  garmin_2972158235,
+  garmin_2969499683,
+  garmin_2969310247
+];
+
+const locations = garminActivities.map(activity => activity.locations);
+
+export default locations;
