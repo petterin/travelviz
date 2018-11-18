e397b43b694cd9de103fff725abb8723dcccafe9
Olga <>
2018-11-18 19:38:19 +0200

    Instagram and countries static data

---
+++ src/data/insta/index.js
@@ -0,0 +1,19 @@
+
+
+const getItems = blob => 
+  blob.data.user.edge_owner_to_timeline_media.edges
+
+const data = [
+  require('./07.json'),
+  require('./06.json'),
+  require('./05.json'),
+  require('./04.json'),
+  require('./03.json'),
+  require('./02.json'),
+  require('./01.json')
+].map(getItems)
+ .reduce((arr, items) => arr.concat(items), [])
+ .reverse()
+ .map(item => item.node)
+
+export default data
